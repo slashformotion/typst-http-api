0a2df70e6d93b8370e348ce570ece523d0a38086
slashformotion <slashformotion@protonmail.com>
2023-05-06 22:39:03 +0200

    Lint

--- typst-http-api/gunicorn.conf.py
+++ typst-http-api/gunicorn.conf.py
@@ -1,12 +1,13 @@
 import multiprocessing
+
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = 2
-loglevel= "debug" 
+loglevel = "debug"
 # valid levels are
-    # 'debug'
-    # 'info'
-    # 'warning'
-    # 'error'
-    # 'critical'
+# - 'debug'
+# - 'info'
+# - 'warning'
+# - 'error'
+# - 'critical'
 
 bind = "0.0.0.0:8000"
